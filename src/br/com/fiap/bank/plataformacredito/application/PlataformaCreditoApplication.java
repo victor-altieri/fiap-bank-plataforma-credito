af8517b9e6418d2946b4f3f877561d84e93564ce
Prof. Eduardo Ramos <profeduardo.ramos@fiap.com.br>
2026-04-24 11:18:35 -0300

    refactor: implement singleton pattern in PlataformaCreditoApplication and PlataformaCreditoPresentation classes

--- src/br/com/fiap/bank/plataformacredito/application/PlataformaCreditoApplication.java
+++ src/br/com/fiap/bank/plataformacredito/application/PlataformaCreditoApplication.java
@@ -1,13 +1,18 @@
 package br.com.fiap.bank.plataformacredito.application;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import br.com.fiap.bank.plataformacredito.domain.entidades.Cliente;
-import br.com.fiap.bank.plataformacredito.domain.entidades.Proposta;
-import br.com.fiap.bank.plataformacredito.domain.valueobjects.CPF;
-
 public class PlataformaCreditoApplication {
+
+    private static PlataformaCreditoApplication instancia;
+
+    private PlataformaCreditoApplication() {
+    }
+
+    public static PlataformaCreditoApplication novaInstancia() {
+        if (instancia == null) {
+            instancia = new PlataformaCreditoApplication();
+        }
+        return instancia;
+    }
 
     public void criarProposta(String codigo, String nome, String dataNascimento, String cpf, String valorSolicitado,
             String quantidadeParcelas) {
